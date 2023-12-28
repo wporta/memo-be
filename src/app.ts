349cdccbbb5ac37035743bb5b1808a240204b7bf
Wilfredo Porta <wporta@gmail.com>
2023-12-28 14:31:41 -0600

    Adding error handling as an Express Middleware.

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,34 @@
 import 'dotenv/config';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import NoteModel from './models/note';
 
 const app = express();
 
-app.get('/', (_req, res) => {
-  res.send('Hello world');
+app.get('/', async (_req, res, next) => {
+  try {
+    const notes = await NoteModel.find().exec();
+    res.status(200).json(notes);
+  } catch (error) {
+    next(error);
+  }
 });
+
+// ****** MiddleWares *******
+
+app.use((_req, _res, next) => {
+  next(Error('Endpoint Not Found'));
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((error: unknown, _req: Request, res: Response, _next: NextFunction) => {
+  console.error(error);
+  let errorMessage = 'An unknown error occurred';
+  if (error instanceof Error) {
+    errorMessage = error.message;
+  }
+  res.status(500).json({ error: errorMessage });
+});
+
+// ****** End of MiddleWares *******
 
 export default app;
