bace091c51dc5d883e9725e16d4fb1d47d2eeadb
Wilfredo Porta <wporta@gmail.com>
2023-12-28 17:17:43 -0600

    Improving error handling.

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express, { NextFunction, Request, Response } from 'express';
 import notesRoutes from './routes/notesRoutes';
 import morgan from 'morgan';
+import createHttpError, { isHttpError } from 'http-errors';
 
 const app = express();
 
@@ -14,17 +15,20 @@ app.use(express.json());
 app.use('/api/notes', notesRoutes);
 
 app.use((_req, _res, next) => {
-  next(Error('Endpoint Not Found'));
+  next(createHttpError(404, 'Endpoint Not Found'));
 });
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((error: unknown, _req: Request, res: Response, _next: NextFunction) => {
   console.error(error);
   let errorMessage = 'An unknown error occurred';
-  if (error instanceof Error) {
+  let statusCode = 500;
+
+  if (isHttpError(error)) {
+    statusCode = error.status;
     errorMessage = error.message;
   }
-  res.status(500).json({ error: errorMessage });
+  res.status(statusCode).json({ error: errorMessage });
 });
 
 // ****** End of MiddleWares *******
