978bf7e32124d5e132bb792ac892ed996bcdec9a
Wilfredo Porta <wporta@gmail.com>
2023-12-28 12:59:08 -0600

    Installing and configuring Moongose.

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,17 @@
 import app from './app';
 import env from './util/validateEnv';
+import mongoose from 'mongoose';
 
 const port = env.PORT;
 
-app.listen(port, () => {
-  console.log(`Server running on port: ${port}`);
-});
+mongoose
+  .connect(env.MONGO_CONNECTION_STRING)
+  .then(() => {
+    console.log('Moongose Connected');
+    app.listen(port, () => {
+      console.log(`Server running on port: ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
